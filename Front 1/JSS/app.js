61d73bea12e78e7327b703c93f59f73deaddd4ba
jppaulussi <70592026+jppaulussi@users.noreply.github.com>
2023-05-25 09:06:52 -0300

    Primeira alteração

---
+++ Front 1/JSS/app.js
@@ -0,0 +1,12 @@
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  var nomeCompleto = document.getElementById("nomeCompleto").value;
+  var endereco = document.getElementById("endereco").value;
+  var itemDoacao = document.getElementById("itemDoacao").value;
+
+  if (nomeCompleto.trim() === '' || endereco.trim() === '' || itemDoacao.trim() === '') {
+    event.preventDefault(); // Impede o envio do formulário
+
+    alert("Por favor, preencha todos os campos."); // Exibe uma mensagem de erro
+  }
+});
